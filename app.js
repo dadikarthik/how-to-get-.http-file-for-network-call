b88d298704e1be5a2b2bb38b2537542b86158edb
dadikarthik <dadikarthik33@gmail.com>
2022-05-21 06:49:33 +0000

    all testcases not passed

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const path = require("path");
-
 const { open } = require("sqlite");
-const sqlite3 = require("sqlite3");
 
-app.use(express.json());
+const sqlite3 = require("sqlite3");
+const path = require("path");
 
 const databasePath = path.join(__dirname, "cricketTeam.db");
 
 const app = express();
+
+app.use(express.json());
 
 let database = null;
 
@@ -41,10 +41,19 @@ const convertDbObjectToResponsiveObject = (dbObject) => {
 app.get("/players/", async (request, response) => {
   const getPlayerQuery = `SELECT * FROM cricket_team;`;
   const playersArray = await database.all(getPlayerQuery);
-  response.send(playersArray.map((eachPlayer) => 
-    convertDbObjectToResponsiveObject(eachPlayer);
+  response.send(
+    playersArray.map((eachPlayer) =>
+      convertDbObjectToResponsiveObject(eachPlayer)
     )
   );
+});
+
+//API 3
+app.get("/players/:playerId/", async (request, response) => {
+  const { playerId } = request.params;
+  const getPlayerQuery = `SELECT * FROM cricket_team WHERE player_id = ${playerId}`;
+  const player = await database.get(getPlayerQuery);
+  response.send(convertDbObjectToResponsiveObject(player));
 });
 
 //API 2
@@ -59,19 +68,11 @@ app.post("/players/", async (request, response) => {
   response.send("Player Added to Team");
 });
 
-//API 3
-app.get("/players/:playerId/", async (request, response) => {
-  const { playerId } = request.params;
-  const getPlayerQuery = `SELECT * FROM cricket_team WHERE player_id = ${playerId}`;
-  const player = await database.get(getPlayerQuery);
-  response.send(convertDbObjectToResponsiveObject(player));
-});
-
 // API 4
 app.put("/players/:playerId/", async (request, response) => {
-  const { playerId } = request.params;
   const { playerName, jerseyNumber, role } = request.body;
-  const updatePlayerDetails = `
+  const { playerId } = request.params;
+  const updatePlayerQuery = `
     UPDATE 
         cricket_team
     SET
@@ -80,7 +81,7 @@ app.put("/players/:playerId/", async (request, response) => {
         role = '${role}
     WHERE
         player_id = ${playerId};`;
-  await database.run(updatePlayerDetails);
+  await database.run(updatePlayerQuery);
   response.send("Player Details Updated");
 });
 
